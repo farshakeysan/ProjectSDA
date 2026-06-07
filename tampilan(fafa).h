1a8ea757168583db71c0d37cbd2d84fc54c78179
syauqi-fafa <fafaalifah7@gmail.com>
2026-06-07 16:11:13 +0000

    Fafa - tambah header tampilan

---
+++ tampilan(fafa).h
@@ -0,0 +1,11 @@
+#ifndef TAMPILAN_H
+#define TAMPILAN_H
+
+#include <string>
+using namespace std;
+
+void cetakJudul(string judul);
+int inputString(const string& label, string& hasil);
+int inputJumlah(double& hasil);
+
+#endif
